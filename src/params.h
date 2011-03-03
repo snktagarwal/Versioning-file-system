67c65a0d62093caed8c2ed8b51d763a090ae5d1c
sanket <sanket@sanketagarwal.com>
2011-03-04 03:17:40 +0530

    Initial FUSE based file system
    The Base Test File system is adopted directly from:
    http://www.cs.nmsu.edu/~pfeiffer/fuse-tutorial/
    The directory structure is cleaned, files renamed and
    Makefile mashed from the original version. Use this as
    the base version.

---
+++ src/params.h
@@ -0,0 +1,25 @@
+// There are a couple of symbols that need to be #defined before
+// #including all the headers.
+
+#ifndef _PARAMS_H_
+#define _PARAMS_H_
+
+// The FUSE API has been changed a number of times.  So, our code
+// needs to define the version of the API that we assume.  As of this
+// writing, the most current API version is 26
+#define FUSE_USE_VERSION 26
+
+// need this to get pwrite().  I have to use setvbuf() instead of
+// setlinebuf() later in consequence.
+#define _XOPEN_SOURCE 500
+
+// maintain vfsfs state in here
+#include <limits.h>
+#include <stdio.h>
+struct vfs_state {
+    FILE *logfile;
+    char *rootdir;
+};
+#define BB_DATA ((struct vfs_state *) fuse_get_context()->private_data)
+
+#endif
