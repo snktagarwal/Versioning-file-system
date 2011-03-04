b8c65ccdbe9f8e2cbbeb6f45174581a425079b7e
sanket <sanket@sanketagarwal.com>
2011-03-04 22:57:25 +0530

    Add glib
    * Added GLib-2.0 for list functions
    * Modified Makefile
    * TODO: Add glib-2.0 to the dependencies

---
+++ src/vfs.h
@@ -0,0 +1,37 @@
+/* vfs.h
+ * Defines the basic data structure and functions defined for the versioning system
+ * does not cover functionality for versioning itself, rather the basic of when
+ * to do versioning
+ * Author: Sanket Agarwal <sanket@sanketagarwal.com
+ */
+
+#include<fuse.h>
+#include<glib.h>
+
+#define MAX_BLOCKS 100
+
+/* Define the types of data that may be stored
+ */
+
+#define WRITE_INFO 1
+// Information for a block
+typedef struct _block_info{
+	
+	int offset;
+	int size;
+}BlockInfo;
+
+// Encapsulates the write info
+typedef struct _write_info{
+	
+	char full_path[255];
+	BlockInfo b_info[MAX_BLOCKS];
+}WriteInfo;
+
+	
+// Versioning info MASTER structure
+typedef struct _ver_info{
+	GSList *ll_wi;	/* Write info list */
+}VerInfo;
+
+
